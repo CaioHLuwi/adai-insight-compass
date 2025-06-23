8cd3626534aecabf390a24d9f4baa0c1de044a93
CaioHLuwi <caiohenriquelw2004@gmail.com>
2025-06-23 04:24:40 -0300

    change logo size

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -134,7 +134,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <a href="/">
-                <img src="/otmizy_logo_darkmode.png" alt="Otmizy.ai Logo" style={{ height: '286px', width: 'auto' }} />
+                <img src="/otmizy_logo_darkmode.png" alt="Otmizy.ai Logo" style={{ height: '150px', width: 'auto' }} />
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
