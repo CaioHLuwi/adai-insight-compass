ca05b31515396ea949480b7afc81995fa61e679c
CaioHLuwi <caiohenriquelw2004@gmail.com>
2025-07-10 17:01:59 -0300

    fix: Change backend url from zeuz to vercel

--- src/services/metaAdsService.ts
+++ src/services/metaAdsService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuração base da API
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://zeuz.otmizy.com/oauth_meta' // Em produção, usar URL relativa
+  ? 'https://otmizy-meta-ads.vercel.app/oauth_meta/health' // Em produção, usar URL relativa
   : 'localhost:8080/oauth_meta'; // Em desenvolvimento, usar URL completa
 
 // Tipos TypeScript para as respostas da API
